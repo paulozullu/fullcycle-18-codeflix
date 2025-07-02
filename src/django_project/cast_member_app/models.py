e36c616d981c756b588efa4a13d51236c47118e0
Paulo Fabrício <paulozullu@gmail.com>
2025-07-02 10:13:55 -0300

    feat: implement Django ORM repository for CastMember

--- src/django_project/cast_member_app/models.py
+++ src/django_project/cast_member_app/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from src.core.cast_member.domain.cast_member import Type
 
-# Create your models here.
+
+class CastMember(models.Model):
+    app_label = "cast_member_app"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=30,
+        choices=[(t.value, t.value) for t in Type],
+    )
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"CastMember(id={self.id}, name={self.name}, type={self.type})"
+
+    class Meta:
+        db_table = "cast_member"
---
+++ src/django_project/cast_member_app/repository.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from src.core.cast_member.domain.cast_member import CastMember
+from src.core.cast_member.domain.cast_member_repository import CastMemberRepository
+from src.django_project.cast_member_app.models import CastMember as CastMemberModel
+
+
+class DjangoORMCastMemberRepository(CastMemberRepository):
+    def __init__(self, model: CastMemberModel = CastMemberModel):
+        self.model = model
+
+    def save(self, cast_member: CastMember):
+        cast_member_model = self.model.objects.create(
+            id=cast_member.id,
+            name=cast_member.name,
+            type=cast_member.type,
+        )
+        cast_member_model.save()
+
+    def get_by_id(self, id: str) -> Optional[CastMember]:
+        try:
+            cast_member_model = self.model.objects.get(id=id)
+            return CastMember(
+                name=cast_member_model.name,
+                id=cast_member_model.id,
+                type=cast_member_model.type,
+            )
+        except self.model.DoesNotExist:
+            return None
+
+    def delete(self, id: str):
+        self.model.objects.filter(id=id).delete()
+
+    def find_all(self) -> list[CastMember]:
+        return [
+            CastMember(
+                id=cast_member_model.id,
+                name=cast_member_model.name,
+                type=cast_member_model.type,
+            )
+            for cast_member_model in self.model.objects.all()
+        ]
+
+    def update(self, cast_member: CastMember):
+        try:
+            self.model.objects.get(id=cast_member.id)
+        except self.model.DoesNotExist:
+            return None
+
+        self.model.objects.filter(id=cast_member.id).update(
+            name=cast_member.name,
+            type=cast_member.type,
+        )

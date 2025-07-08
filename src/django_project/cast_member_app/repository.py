3e258936b6893d6752590dc78db4be8fab9110cc
Paulo Fabrício <paulozullu@gmail.com>
2025-07-08 08:55:16 -0300

    refactor: streamline cast member repository methods using CastMemberModelMapper

--- src/django_project/cast_member_app/repository.py
+++ src/django_project/cast_member_app/repository.py
@@ -10,21 +10,13 @@ class DjangoORMCastMemberRepository(CastMemberRepository):
         self.model = model
 
     def save(self, cast_member: CastMember):
-        cast_member_model = self.model.objects.create(
-            id=cast_member.id,
-            name=cast_member.name,
-            type=cast_member.type,
-        )
+        cast_member_model = CastMemberModelMapper.to_model(cast_member)
         cast_member_model.save()
 
     def get_by_id(self, id: str) -> Optional[CastMember]:
         try:
             cast_member_model = self.model.objects.get(id=id)
-            return CastMember(
-                name=cast_member_model.name,
-                id=cast_member_model.id,
-                type=Type(cast_member_model.type),
-            )
+            return CastMemberModelMapper.to_entity(cast_member_model)
         except self.model.DoesNotExist:
             return None
 
@@ -33,11 +25,7 @@ class DjangoORMCastMemberRepository(CastMemberRepository):
 
     def find_all(self) -> list[CastMember]:
         return [
-            CastMember(
-                id=cast_member_model.id,
-                name=cast_member_model.name,
-                type=Type(cast_member_model.type),
-            )
+            CastMemberModelMapper.to_entity(cast_member_model)
             for cast_member_model in self.model.objects.all()
         ]
 
@@ -50,4 +38,22 @@ class DjangoORMCastMemberRepository(CastMemberRepository):
         self.model.objects.filter(id=cast_member.id).update(
             name=cast_member.name,
             type=cast_member.type,
+        )
+
+
+class CastMemberModelMapper:
+    @staticmethod
+    def to_model(cast_member: CastMember) -> CastMemberModel:
+        return CastMemberModel(
+            id=cast_member.id,
+            name=cast_member.name,
+            type=cast_member.type,
+        )
+
+    @staticmethod
+    def to_entity(cast_member_model: CastMemberModel) -> CastMember:
+        return CastMember(
+            id=cast_member_model.id,
+            name=cast_member_model.name,
+            type=Type(cast_member_model.type),
         )
